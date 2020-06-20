e105bfa5ce04fe3a7f629845a36634b88a2438fd
Ali Gamal Ali <hictors@gmail.com>
2020-06-20 17:09:28 +0200

    first View of the full site

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -5,23 +5,20 @@ const navList = document.querySelector(".nav__list");
 navNames.forEach((element) => {
   const navItem = document.createElement("li");
   navItem.classList = "nav__item";
-  navItem.dataset.link = element.dataset.nav;
-  navItem.innerHTML = `<a class="nav__link" href="#${element.id}">${element.dataset.nav}</a>`;
+  navItem.innerHTML = `<a class="nav__link" href="#${element.id}" data-link="${element.dataset.nav}">${element.dataset.nav}</a>`;
   navItem.addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
-    
+
     // If there's no active class
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" active", "");
     }
-    
+
     // Add the active class to the clicked navItem
     this.classList.add("active");
   });
   navList.appendChild(navItem);
 });
-
-
 
 let start = 150;
 const onPageScroll = function () {
@@ -29,9 +26,16 @@ const onPageScroll = function () {
   if (window.pageYOffset > start) {
     header.style.backgroundColor = "#333";
     header.style.height = "10em";
+    header.style.opacity = 0.8;
+    header.addEventListener("mouseover", function () {
+      header.style.opacity = 1;
+    });
   } else {
     header.style.backgroundColor = "#fff";
     header.style.height = "15em";
+    header.addEventListener("mouseleave", function () {
+      header.style.opacity = 0.8;
+    });
   }
 };
 
